431ba4c2d9f00ca91ba719dc94561687154b8927
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2019-11-18 14:49:50 +0000

    add pets model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pets/index'
+  get 'pets/show'
+  get 'pets/new'
+  get 'pets/create'
+  get 'pets/edit'
+  get 'pets/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_134603) do
+ActiveRecord::Schema.define(version: 2019_11_18_141246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "pets", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name", null: false
+    t.string "description"
+    t.string "photo"
+    t.string "address"
+    t.integer "price"
+    t.string "animal_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "address", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
     t.string "description"
     t.string "photo"
     t.string "email", default: "", null: false
@@ -32,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_11_18_134603) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pets", "users"
 end
