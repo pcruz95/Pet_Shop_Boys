c5d67b60cbbc44a50184abe43c04bb44bcbff1d0
Pedro Cruz <pedro_cruz_1995@hotmail.com>
2019-11-21 16:19:53 +0000

    navbar transition

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,9 @@ import "./application.css"
 import { initFlatPickr } from '../plugins/flatpickr'
 import { initMapbox } from "../plugins/init_mapbox.js";
 import { getPosition } from "../plugins/mapbox_position.js";
+import { scrollNavbar } from "../plugins/scroll_navbar.js";
 
+scrollNavbar();
 initMapbox();
 getPosition();
 initFlatPickr();
