e14a21e130aad367e4a213eb40bf1fc05f49670e
Pedro Cruz <pedro_cruz_1995@hotmail.com>
2019-11-21 19:12:27 +0000

    mapbox zoom fixed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,7 +10,7 @@ const initMapbox = (location) => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10',
       center: location,
-      zoom: 11
+      zoom: 12
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
