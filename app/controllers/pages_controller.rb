dcf8549ff067363312c3cc6676b30c497ce76a75
Pedro Cruz <pedro_cruz_1995@hotmail.com>
2019-11-22 10:47:55 +0000

    mainpage map fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
     @pets = policy_scope(Pet)
+    @markers = []
+    @pets.each do |pet|
+      @markers << {lat: pet.latitude, lng: pet.longitude}
+    end
+    @markers
   end
 end
