7fa15e443bbb3f44e3131eda4cadce328e48d08c
Pedro Cruz <pedro_cruz_1995@hotmail.com>
2019-11-22 12:19:53 +0000

    main page markers popups

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     @pets = policy_scope(Pet)
     @markers = []
     @pets.each do |pet|
-      @markers << {lat: pet.latitude, lng: pet.longitude}
+      @markers << {lat: pet.latitude, lng: pet.longitude, infoWindow: render_to_string(partial: "pets/info_window", locals: { pet: pet })}
     end
     @markers
   end
